b7b84807ac7afe263c169015ea9d8f64f8d80577
Rui Furtado <furtado911@gmail.com>
2017-04-09 19:01:01 +0100

    Added come code to control panel not working yet

--- src/main/java/org/academiadecodigo/bootcamp/controller/PanelController.java
+++ src/main/java/org/academiadecodigo/bootcamp/controller/PanelController.java
@@ -1,14 +1,20 @@
 package org.academiadecodigo.bootcamp.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.academiadecodigo.bootcamp.Navigation;
-
+import org.academiadecodigo.bootcamp.model.Event;
+import org.academiadecodigo.bootcamp.service.event.EventService;
 
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -18,6 +24,24 @@ public class PanelController implements Initializable {
 
     @FXML
     private TableView enlistedtable;
+
+    @FXML
+    private TableColumn localcolumn;
+
+    @FXML
+    private TableColumn typecolumn;
+
+    @FXML
+    private TableColumn periodcolumn;
+
+    @FXML
+    private TableColumn nplayerscolumn;
+
+    private EventService eventService;
+
+    public void setEventService(EventService eventService) {
+        this.eventService = eventService;
+    }
 
     public Navigation getNavigation() {
         return navigation;
@@ -47,6 +71,34 @@ public class PanelController implements Initializable {
     }
 
     public void viewenlisted(ActionEvent actionEvent) {
+
         enlistedtable.setVisible(true);
+
+        List<Event> eventstotal = eventService.findAll();
+
+        ObservableList<Event> data = FXCollections.observableArrayList();
+
+        for (Event el : eventstotal) {
+            data.add(el);
+        }
+
+        localcolumn.setCellValueFactory(new PropertyValueFactory<Event, String>("local"));
+
+
+        typecolumn.setCellValueFactory(
+                new PropertyValueFactory<Event, String>("type")
+        );
+
+        periodcolumn.setCellValueFactory(
+                new PropertyValueFactory<Event, String>("period")
+        );
+
+        nplayerscolumn.setCellValueFactory(
+                new PropertyValueFactory<Event, Integer>("numberOfPlayers")
+        );
+
+        enlistedtable.setItems(data);
+
     }
 }
+
